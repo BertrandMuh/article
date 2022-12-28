aee703499fa963af6a38082cc15de1c9b852ce42
BertrandMuh <sangwabertrand14@gmail.com>
2022-12-28 11:07:45 -0600

    modify js and css

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const displayForm = () => {
     let form = document.getElementById('form')
     form.classList.toggle('form-show');
-    console.log(form)
 }
 
-let navbar = document.getElementsByClassName('navbar')[0];
-const elementPressed = el => {
+const changePageTitleContent = el => {
     const pageTitle = document.getElementsByClassName('title')[0];
+    //check if the element clicked is an anchor
     const isAnchor = el.target.nodeName === 'A';
     if (isAnchor) {
+        //change the content of the title if true
         pageTitle.innerHTML = el.target.innerHTML
     }
 }
-navbar.addEventListener("click", elementPressed);
+let navbar = document.getElementsByClassName('navbar')[0];
+navbar.addEventListener("click", changePageTitleContent);
+let searchIcon = document.getElementsByClassName('fa-search')[0];
+searchIcon.addEventListener("click", displayForm);
