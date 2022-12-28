014f743c6f213eede5cc9bca06907d1a87d294b4
BertrandMuh <sangwabertrand14@gmail.com>
2022-12-27 22:09:26 -0600

    add first event listener to change the header text

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const displayForm = () => {
     form.classList.toggle('form-show');
     console.log(form)
 }
+
+let navbar = document.getElementsByClassName('navbar')[0];
+const elementPressed = el => {
+    const pageTitle = document.getElementsByClassName('title')[0];
+    const isAnchor = el.target.nodeName === 'A';
+    if (isAnchor) {
+        pageTitle.innerHTML = el.target.innerHTML
+    }
+}
+navbar.addEventListener("click", elementPressed);
